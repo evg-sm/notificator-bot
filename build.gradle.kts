1c5e3e1b8572ae8ad71859f27484f5a2bbddb5ab
evg-sm <esemin83@gmail.com>
2023-04-01 23:29:57 +0300

    initial commit;

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    val kotlinVersion = "1.7.10"
+
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+}
+
+group = "com.notificator.bot"
+version = "0.0.1-SNAPSHOT"
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // NotificatorBotApp
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // db
+    implementation("com.h2database:h2")
+    implementation("org.flywaydb:flyway-core")
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    // telegram
+    implementation("org.telegram:telegrambots:6.5.0")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/resources/db/migration/V1.0.1__create_table.sql
@@ -0,0 +1,5 @@
+create table if not exists mytable
+(
+    uuid varchar(50) primary key not null,
+    name varchar(50)             not null
+);
